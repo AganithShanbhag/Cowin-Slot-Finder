7bdc51b68bc7a862286f4e1e835ffa4dd2fce7c0
Aganith Shanbhag <aganith.shanbhag@learner.manipal.edu>
2021-06-09 14:11:44 +0530

    5th Commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app"
 import "firebase/auth"
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyBMeZ37uT7FJ-yHyt1JvDCsIboExTZbTDg",
+  authDomain: "cowin-slot-16444.firebaseapp.com",
+  projectId: "cowin-slot-16444",
+  storageBucket: "cowin-slot-16444.appspot.com",
+  messagingSenderId: "1089755110755",
+  appId: "1:1089755110755:web:cdb7f281b4c0b7c0b9c756"
 })
 
 export const auth = app.auth()
